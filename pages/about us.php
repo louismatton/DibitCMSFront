3e6f913f6f72f6ffbb7c28f8182391a98977cc30
Louis Matton <louis.matton@outlook.com>
2018-06-11 16:03:07 +0200

    cleanup fix imges

--- pages/about us.php
+++ pages/about us.php
@@ -6,51 +6,23 @@ $paginaTitle="about us";
 $paginaTitleQuery=str_replace(" ","%20",$paginaTitle);
 
 $json = file_get_contents("http://localhost:3000/website/website1");
+$jsonDecoded=json_decode($json);
 
-$jsonIterator = new RecursiveIteratorIterator(new RecursiveArrayIterator(json_decode($json, TRUE)),RecursiveIteratorIterator::SELF_FIRST);
-$arrpages=array();
-$arrpagesVis=array();
+$orderedPages=$jsonDecoded->pages;
 
-foreach($jsonIterator as $key => $val) {
-    if ($key === "title") {
-        // $title;
-        $title=$val;
-    }
-    if ($key === "pages") {
-        // print_r($val[0]);
-        foreach($val as $pageKey => $page) {
-            // if ($pageKey === "pageTitle") {
-            // print_r($page);
-            // echo "<br><br>";
-            foreach($page as $pageel => $valueP) {
-                if($pageel === "pageVisible"){
-                    // echo $valueP;
-                    if($valueP==true){
+usort($orderedPages, function($a, $b) { //Sort the array using a user defined function
+    return $a->pageOrder > $b->pageOrder ? 1 : -1; //Compare the scores
+});
 
-                        array_push($arrpagesVis, "true");
-                    }else{
-                        array_push($arrpagesVis, "false");                        
-                    }
-
-                }
-                if ($pageel === "pageTitle") {
-                    // echo $valueP;
-                    array_push($arrpages, $valueP);
-                }
-            }
-        }
-    }
-
-}
 ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style.css" >
     <link rel="icon" type="image/x-icon" href="../assets/logodibit.png">
 
-    <title><?php echo $title." | ".$paginaTitle;?></title>
+    <title><?php echo $jsonDecoded->title." | ".$paginaTitle;?></title>
 </head>
 <body>
 <header>
@@ -63,79 +35,59 @@ foreach($jsonIterator as $key => $val) {
             </div>
             <ul>
                 <?php
-                for ($i = 0; $i < count($arrpages); $i++) {
-                    // echo $arrpagesVis[$i];
-                    if ($arrpagesVis[$i] != "false"){
-                        if (strtolower($arrpages[$i]) == "home") {
-                            echo '<li><a href="./index">'.$arrpages[$i].'</a></li>';
-                        } else {
-                            echo '<li><a href="./'.$arrpages[$i].'">'.$arrpages[$i].
-                            '</a></li>';
-                        }
+                foreach($orderedPages as $page){
+                    if($page->pageVisible){
+                        if (strtolower($page->pageTitle) == "home"&& strtolower($page->pageTitle)==strtolower($paginaTitle)) {
+                            echo '<li class="selected"><a href="./index">'.$page->pageTitle.'</a></li>';
+                        } else if(strtolower($page->pageTitle) == "home"&& strtolower($page->pageTitle)!=strtolower($paginaTitle)){
+                            echo '<li ><a href="./index">'.$page->pageTitle.'</a></li>';
 
+                        } else if(strtolower($page->pageTitle)==strtolower($paginaTitle)){
+                            echo '<li class="selected"><a href="./'.$page->pageTitle.'">'.$page->pageTitle.
+                            '</a></li>';
+                        }else{
+                            echo '<li ><a href="./'.$page->pageTitle.'">'.$page->pageTitle.
+                            '</a></li>';
+
+                        }
                     }
                 }
                 ?>
             </ul>
         </div>
     </div>
-
 </header>
 <div id="body">
 
 <?php
 $jsonContent = file_get_contents("http://localhost:3000/website/website1/".$paginaTitleQuery);
+$data = json_decode($jsonContent); // decode the JSON feed
 
-$jsonIteratorContent = new RecursiveIteratorIterator(new RecursiveArrayIterator(json_decode($jsonContent, TRUE)),RecursiveIteratorIterator::SELF_FIRST);
 echo '<div id="posts" >';
 
-foreach($jsonIteratorContent as $key => $val) {
 
-    if ($key === "posts") {
-        // echo "$key: ";
-        // echo '<div style="border: 2px solid green; padding:5px;">';
-        $tel = 0;
-        $arrVis = array();
-        foreach($val as $postKey => $post) {
-            // echo '<div style="border: 2px solid blue; padding:5px;">';
-            // echo '<div class="post">';
-            foreach($post as $postKey1 => $postval) {
-                if ($postKey1 === "postVisible") {
-                    // echo "*".$postval;
-                    if ($postval == 1) {
-                        array_push($arrVis, "true");
+// echo $characters;
+// echo serialize($characters->posts);
+$orderedPosts=$data->posts;
+usort($orderedPosts, function($a, $b) { //Sort the array using a user defined function
+    return $a->postOrder > $b->postOrder ? 1 : -1; //Compare the scores
+});
+foreach($orderedPosts as $post){
+    if($post->postVisible){
+        echo '<section class="post">';
+        echo '<h2 class="postTitle">'.$post->postTitle.'</h2>';
+        echo '<p class="postText">'.nl2br($post->postText).'</p>';
 
-                    } else {
-                        array_push($arrVis, "false");
-                    }
-                }
-            }
-
-            if (isset($arrVis[$tel]) && $arrVis[$tel] == "true") {
-                echo '<section class="post">';
-
-                foreach($post as $postKey1 => $postval) {
-                    if ($postKey1 === "postTitle") {
-                        echo '<h2 class="postTitle">'.$postval.'</h2>';
-                    }
-                    if ($postKey1 === "postText") {
-                        $postval = str_replace("\n", "<br>", $postval);
-                        echo '<p class="postText">'.$postval.'</p>';
-                    }
-
-                }
-                echo '<hr>';
-                echo '</section>';
-            }
-            // }
-            $tel++;
+        foreach($post->postPhotos as $photo){
+            echo '<img src="'.$photo.'" class="image" >';
         }
-        // echo '</div>';
+        echo '<hr>';
+        echo '</section>';
     }
-
 }
 echo '</div>';
 ?>
+
 </div>
 </body>
 </html>
--- pages/index.php
+++ pages/index.php
@@ -6,41 +6,23 @@ $paginaTitle="home";
 $paginaTitleQuery=str_replace(" ","%20",$paginaTitle);
 
 $json = file_get_contents("http://localhost:3000/website/website1");
+$jsonDecoded=json_decode($json);
 
-$jsonIterator = new RecursiveIteratorIterator(new RecursiveArrayIterator(json_decode($json, TRUE)),RecursiveIteratorIterator::SELF_FIRST);
-$arrpages=array();
-$arrpagesVis=array();
+$orderedPages=$jsonDecoded->pages;
 
-foreach($jsonIterator as $key => $val) {
-    if ($key === "title") {
-        $title=$val;
-    }
-    if ($key === "pages") {
-        foreach($val as $pageKey => $page) {
-            foreach($page as $pageel => $valueP) {
-                if($pageel === "pageVisible"){
-                    if($valueP==true){
-                        array_push($arrpagesVis, "true");
-                    }else{
-                        array_push($arrpagesVis, "false");                        
-                    }
-                }
-                if ($pageel === "pageTitle") {
-                    array_push($arrpages, $valueP);
-                }
-            }
-        }
-    }
-}
+usort($orderedPages, function($a, $b) { //Sort the array using a user defined function
+    return $a->pageOrder > $b->pageOrder ? 1 : -1; //Compare the scores
+});
+
 ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style.css" >
     <link rel="icon" type="image/x-icon" href="../assets/logodibit.png">
 
-    <title><?php echo $title." | ".$paginaTitle;?></title>
+    <title><?php echo $jsonDecoded->title." | ".$paginaTitle;?></title>
 </head>
 <body>
 <header>
@@ -53,14 +35,20 @@ foreach($jsonIterator as $key => $val) {
             </div>
             <ul>
                 <?php
-                for ($i = 0; $i < count($arrpages); $i++) {
-                    // echo $arrpagesVis[$i];
-                    if ($arrpagesVis[$i] != "false"){
-                        if (strtolower($arrpages[$i]) == "home") {
-                            echo '<li><a href="./index">'.$arrpages[$i].'</a></li>';
-                        } else {
-                            echo '<li><a href="./'.$arrpages[$i].'">'.$arrpages[$i].
+                foreach($orderedPages as $page){
+                    if($page->pageVisible){
+                        if (strtolower($page->pageTitle) == "home"&& strtolower($page->pageTitle)==strtolower($paginaTitle)) {
+                            echo '<li class="selected"><a href="./index">'.$page->pageTitle.'</a></li>';
+                        } else if(strtolower($page->pageTitle) == "home"&& strtolower($page->pageTitle)!=strtolower($paginaTitle)){
+                            echo '<li ><a href="./index">'.$page->pageTitle.'</a></li>';
+
+                        } else if(strtolower($page->pageTitle)==strtolower($paginaTitle)){
+                            echo '<li class="selected"><a href="./'.$page->pageTitle.'">'.$page->pageTitle.
                             '</a></li>';
+                        }else{
+                            echo '<li ><a href="./'.$page->pageTitle.'">'.$page->pageTitle.
+                            '</a></li>';
+
                         }
                     }
                 }
@@ -73,52 +61,30 @@ foreach($jsonIterator as $key => $val) {
 
 <?php
 $jsonContent = file_get_contents("http://localhost:3000/website/website1/".$paginaTitleQuery);
+$data = json_decode($jsonContent); // decode the JSON feed
 
-$jsonIteratorContent = new RecursiveIteratorIterator(new RecursiveArrayIterator(json_decode($jsonContent, TRUE)), RecursiveIteratorIterator::SELF_FIRST);
 echo '<div id="posts" >';
 
-foreach($jsonIteratorContent as $key => $val) {
 
-    if ($key === "posts") {
-        $tel = 0;
-        $arrVis = array();
-        foreach($val as $postKey => $post) {
-            foreach($post as $postKey1 => $postval) {
-                if ($postKey1 === "postVisible") {
-                    if ($postval == 1) {
-                        array_push($arrVis, "true");
+// echo $characters;
+// echo serialize($characters->posts);
+$orderedPosts=$data->posts;
+usort($orderedPosts, function($a, $b) { //Sort the array using a user defined function
+    return $a->postOrder > $b->postOrder ? 1 : -1; //Compare the scores
+});
+foreach($orderedPosts as $post){
+    if($post->postVisible){
+        echo '<section class="post">';
+        echo '<h2 class="postTitle">'.$post->postTitle.'</h2>';
+        echo '<p class="postText">'.nl2br($post->postText).'</p>';
 
-                    } else {
-                        array_push($arrVis, "false");
-                    }
-                }
-            }
-
-            if (isset($arrVis[$tel]) && $arrVis[$tel] == "true") {
-                echo '<section class="post">';
-
-                foreach($post as $postKey1 => $postval) {
-                    if ($postKey1 === "postTitle") {
-                        echo '<h2 class="postTitle">'.$postval.'</h2>';
-                    }
-                    if ($postKey1 === "postText") {
-                        $postval = str_replace("\n", "<br>", $postval);
-                        echo '<p class="postText">'.$postval.'</p>';
-                    }
-                    if ($postKey1 === "postPhotos") {
-                        for ($i = 0; $i < count($postval); $i++) {
-                            echo '<img src="'.$postval[$i].'">';
-                        }
-                    }
-                }
-                echo '<hr>';
-                echo '</section>';
-            }
-            $tel++;
+        foreach($post->postPhotos as $photo){
+            echo '<img src="'.$photo.'" class="image" >';
         }
+        echo '<hr>';
+        echo '</section>';
     }
 }
-
 echo '</div>';
 ?>
 
